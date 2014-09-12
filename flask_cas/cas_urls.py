9d7107af1ce69809bdbdd8e3febfecec30b3d3c3
Cameron Brandon White <cameron.white@janrain.com>
2014-09-12 11:16:52 -0700

    Added create functions for missing version 2/3 urls

--- flask_cas/cas_urls.py
+++ flask_cas/cas_urls.py
@@ -47,7 +47,8 @@ def create_url(base, path=None, *query):
     return url
 
 
-def create_cas_login_url(cas_url, cas_route, service, renew=None, gateway=None):
+def create_cas_login_url(cas_url, cas_route, service, renew=None,
+                         gateway=None, method=None):
     """ Create a CAS login URL .
 
     Keyword arguments:
@@ -71,6 +72,7 @@ def create_cas_login_url(cas_url, cas_route, service, renew=None, gateway=None):
         ('service', service),
         ('renew', renew),
         ('gateway', gateway),
+        ('method', method),
     )
 
 
@@ -126,4 +128,115 @@ def create_cas_validate_url(cas_url, cas_route, service, ticket,
         ('service', service),
         ('ticket', ticket),
         ('renew', renew),
+    )
+
+def create_cas_serviceValidate_url(cas_url, cas_route, service, ticket,
+                                   pgtUrl=None, renew=None):
+    
+    """ Create a CAS serviceValidate URL.
+
+    Keyword arguments:
+    cas_url -- The url to the CAS (ex. http://sso.pdx.edu)
+    cas_route -- The route where the CAS lives on server (ex.  /cas/serviceValidate)
+    service -- (ex.  http://localhost:5000/login)
+    ticket -- (ex. 'ST-58274-x839euFek492ou832Eena7ee-cas')
+    pgtUrl -- The url of the proxy callback
+    renew -- "true" or "false"
+
+    Example usage:
+    >>> create_cas_serviceValidate_url(
+    ...     'http://sso.pdx.edu',
+    ...     '/cas/serviceValidate',
+    ...     'http://localhost:5000/login',
+    ...     'ST-58274-x839euFek492ou832Eena7ee-cas'
+    ... )
+    'http://sso.pdx.edu/cas/serviceValidate?service=http%3A%2F%2Flocalhost%3A5000%2Flogin&ticket=ST-58274-x839euFek492ou832Eena7ee-cas'
+    """
+    return create_url(
+        cas_url,
+        cas_route,
+        ('service', service),
+        ('ticket', ticket),
+        ('pgtUrl', pgtUrl),
+        ('renew', renew),
+    )
+
+def create_cas_proxyValidate_url(cas_url, cas_route, service, ticket,
+                                 pgtUrl=None, renew=None):
+    
+    """ Create a CAS proxyValidate URL.
+
+    Keyword arguments:
+    cas_url -- The url to the CAS (ex. http://sso.pdx.edu)
+    cas_route -- The route where the CAS lives on server (ex.  /cas/proxyValidate)
+    service -- (ex.  http://localhost:5000/login)
+    ticket -- (ex. 'ST-58274-x839euFek492ou832Eena7ee-cas')
+    pgtUrl -- The url of the proxy callback
+    renew -- "true" or "false"
+
+    Example usage:
+    >>> create_cas_proxyValidate_url(
+    ...     'http://sso.pdx.edu',
+    ...     '/cas/proxyValidate',
+    ...     'http://localhost:5000/login',
+    ...     'ST-58274-x839euFek492ou832Eena7ee-cas'
+    ... )
+    'http://sso.pdx.edu/cas/proxyValidate?service=http%3A%2F%2Flocalhost%3A5000%2Flogin&ticket=ST-58274-x839euFek492ou832Eena7ee-cas'
+    """
+    return create_url(
+        cas_url,
+        cas_route,
+        ('service', service),
+        ('ticket', ticket),
+        ('pgtUrl', pgtUrl),
+        ('renew', renew),
+    )
+
+def create_cas_proxy_url(cas_url, cas_route, pgt, targetService):
+
+    """ Create a CAS proxy URL.
+
+    Keyword arguments:
+    cas_url -- The url to the CAS (ex. http://sso.pdx.edu)
+    cas_route -- The route where the CAS lives on server (ex.  /cas/proxy)
+    pgt -- The proxy-granting ticket
+    targetService -- The service identifier of the back-end service
+
+    Example usage:
+    >>> create_cas_proxy_url(
+    ...     'http://sso.pdx.edu',
+    ...     '/cas/proxy',
+    ...     'PGT-490649-W81Y9Sa2vTM7hda7xNTkezTbVge4CUsybAr',
+    ...     'http://www.service.com',
+    ... )
+    'http://sso.pdx.edu/cas/proxy?pgt=PGT-490649-W81Y9Sa2vTM7hda7xNTkezTbVge4CUsybAr&targetService=http%3A%2F%2Fwww.service.com'
+    """
+    return create_url(
+        cas_url,
+        cas_route,
+        ('pgt', pgt),
+        ('targetService', targetService),
+    )
+
+def create_cas_samIValidate_url(cas_url, cas_route, target):
+
+    """ Create a CAS samIValidate URL.
+
+    Keyword arguments:
+    cas_url -- The url to the CAS (ex. http://sso.pdx.edu)
+    cas_route -- The route where the CAS lives on server (ex.  /cas/proxy)
+    target -- The url of the back-end service
+
+    Example usage:
+    >>> create_cas_samIValidate_url(
+    ...     'http://sso.pdx.edu',
+    ...     '/cas/samIValidate',
+    ...     'http://www.target.com',
+    ... )
+    'http://sso.pdx.edu/cas/samIValidate?target=http%3A%2F%2Fwww.target.com'
+    """
+    return create_url(
+        cas_url,
+        cas_route,
+        ('target', target),
     )
